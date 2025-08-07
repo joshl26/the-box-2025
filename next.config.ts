5503b46c2a37ed64c43a55249d3aef523b8d16b2
joshl26 <joshl26@hotmail.com>
2025-08-07 13:50:58 -0400

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["http://localhost:3000"], // Replace with your external domain
+    domains: ["http://localhost:3000/"], // Replace with your external domain
   },
 };
 
